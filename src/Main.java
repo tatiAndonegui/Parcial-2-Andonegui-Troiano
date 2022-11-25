fa822a9fbe4662b47df2a1775a9531e523afcbb4
tatiAndonegui <115169557+tatiAndonegui@users.noreply.github.com>
2022-11-24 22:07:09 -0300

    chore: Create objetcs in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        Shape s1 = new Circle(5.5,"red",false);
+
+        Shape s2 = new Circle();
+        s2.setColor("red");
+
+        Shape s3 = new Rectangle(1.0, 2.0, "red", false);
+
+        Shape s4 = new Square(6.6);
+
+
+        System.out.println(s1);
+        System.out.println("El color del circulo es " + s2.getColor());
+        System.out.println(s3);
+        System.out.println(s4);
+
 
     }
+
 }
